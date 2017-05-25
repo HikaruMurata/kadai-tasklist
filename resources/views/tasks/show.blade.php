a4ddfb2c2fa8b4ca0f471616310c428e6908b8ae
Hikaru <villagepika2@yahoo.co.jp>
2017-05-25 14:32:23 +0000

    tasklist

---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+<h1>id = {{ $tasks->id }} のタスク詳細ページ</h1>
+    
+    <p>{{ $tasks->content }}</p>
+     {!! link_to_route('tasks.edit', 'このメッセージ編集', ['id' => $tasks->id]) !!}
+      {!! Form::model($tasks, ['route' => ['tasks.destroy', $tasks->id], 'method' => 'delete']) !!}
+        {!! Form::submit('削除') !!}
+    {!! Form::close() !!}
+
+@endsection
