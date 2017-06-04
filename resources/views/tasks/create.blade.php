ec2c644a145e8eb0615157ccacddd3e67bc8ebb3
Hikaru <villagepika2@yahoo.co.jp>
2017-06-04 07:48:12 +0000

    Bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,19 +6,24 @@
 <h1>タスク新規作成ページ</h1>
 
     @include('commons.error_tasks')
-   
-
-    {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
+     <div class="col-xs-12" "col-sm-offset-2 col-sm-8" "col-md-offset-2 col-md-8" "col-lg-offset-3 col-lg-6">
+    
+      {!! Form::model($tasks, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status') !!}
+                </div>
+                    
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク:') !!}
+                    {!! Form::text('content') !!}
+                </div>
+    </div>
+                
+                {!! Form::submit('投稿') !!}
         
-        
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('投稿') !!}
-      
-    {!! Form::close() !!}
+            {!! Form::close() !!}
+            
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -6,19 +6,25 @@
 <h1>id: {{ $tasks->id }} のメッセージ編集ページ</h1>
 
     @include('commons.error_tasks')
-
-
-    {!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-
-        {!! Form::label('content', 'タスク:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('更新') !!}
-
-    {!! Form::close() !!}
+    
+    
+    <div class="row">
+        <div class="col-xs-12" "col-sm-offset-2 col-sm-8" "col-md-offset-2 col-md-8" "col-lg-offset-3 col-lg-6">
+            {!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
+                <div class="form-group">
+                    {!! Form::label('status', 'ステータス:') !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                <div class="form-group">
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+        
+                {!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+        
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
