825b31899d4ca049ce434b735985103913d91299
Hikaru <villagepika2@yahoo.co.jp>
2017-06-04 13:44:57 +0000

    bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -12,12 +12,12 @@
             
                 <div class="form-group">
                     {!! Form::label('status', 'ステータス:') !!}
-                    {!! Form::text('status' null, ['class' => 'form-control']) !!}
+                    {!! Form::text('status', null, ['class' => 'form-control']) !!}
                 </div>
                     
                 <div class="form-group">
                     {!! Form::label('content', 'タスク:') !!}
-                    {!! Form::text('content' null, ['class' => 'form-control']) !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
     </div>
                 
