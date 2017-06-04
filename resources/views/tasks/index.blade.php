57adcf53fdac44911ca3f0388d72e4786956ce78
Hikaru <villagepika2@yahoo.co.jp>
2017-06-04 13:37:13 +0000

    bootstrap

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -18,9 +18,9 @@
             <tbody>
                 @foreach ($tasks as $task)
                     <tr>
-                        <td>{!! link_to_route('tasks.show', $tasks->id, ['id' => $tasks->id]) !!}</td>
-                        <td>{{ $tasks->status }}</td>
-                        <td>{{ $tasks->content }}</td>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->status }}</td>
+                        <td>{{ $task->content }}</td>
                     </tr>
                 @endforeach
             </tbody>
